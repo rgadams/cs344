20cfbee1bead53de343783b0081cbc4ad7dfec75
rgadams <roy.g.adams@gmail.com>
2019-05-03 10:07:44 -0400

    Adding lab13

---
+++ lab13/lab_1.pl
@@ -0,0 +1,24 @@
+% Problem 3.2
+directlyIn(katarina, olga).
+directlyIn(olga, natasha).
+directlyIn(natasha, irina).
+
+in(X, Y) :- directlyIn(X, Y).
+in(X, Y) :- directlyIn(X, Z),
+    in(Z, Y).
+
+% Problem 4.5
+tran(eins,one).
+tran(zwei,two).
+tran(drei,three).
+tran(vier,four).
+tran(fuenf,five).
+tran(sechs,six).
+tran(sieben,seven).
+tran(acht,eight).
+tran(neun,nine).
+
+listtran([], []).
+listtran([G|TG], [E|TE]) :- tran(G, E), listtran(TG, TE).
+
+% Part b)
